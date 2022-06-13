f5db902353cf3b5b1566db9e841c3a531fd70e70
thiagoaderaldo <thiagoaderaldo@gmail.com>
2022-06-13 11:21:56 -0300

    Módulo 2 - Adicição de uma fonte externa ao projeto

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
-import { StatusBar, SafeAreaView, View } from 'react-native';
-import Cesta from './src/telas/Cesta';
+import { StatusBar, SafeAreaView, View } from "react-native";
+import Cesta from "./src/telas/Cesta";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
 
 export default function App() {
+
+  const [fonteCarregada] = useFonts({
+    "MontsserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  })
+
+  if(!fonteCarregada){
+    return <View />    
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
@@ -9,5 +24,3 @@ export default function App() {
     </SafeAreaView>
   );
 }
-
-
