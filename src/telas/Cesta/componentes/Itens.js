66cad19515f9613e36f0cc7819d6b604486285cd
thiagoaderaldo <thiagoaderaldo@gmail.com>
2022-06-14 14:59:24 -0300

    Módulo 4 - Implementação do componente Itens

---
+++ src/telas/Cesta/componentes/Itens.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+import Texto from "../../componentes/Texto";
+
+export default function Itens({ titulo, lista }) {
+  return (
+    <>
+      <Texto style={estilos.titulo}>{titulo}</Texto>
+      {lista.map(({ nome, imagem }) => {
+        return (
+          <View key={nome} style={estilos.item}>
+            <Image source={imagem} style={estilos.imagem}/>
+            <Texto style={estilos.nome}>{nome}</Texto>
+          </View>
+        );
+      })}
+    </>
+  );
+}
+
+const estilos = StyleSheet.create({
+  titulo: {
+    color: "#464646",
+    fontWeight: "bold",
+    marginTop: 32,
+    marginBottom: 5,
+    fontSize: 20,
+    lineHeight: 32,
+  },
+  item: {
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ECECEC",
+    paddingVertical: 16,
+    alignItems: "center",
+  },
+  imagem: {
+    width: 46,
+    height: 46,
+  },
+  nome: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 11,
+    color: "#464646",
+  }  
+})
--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import Detalhes from "./componentes/Detalhes";
+import Itens from "./componentes/Itens";
 
 import Topo from "./componentes/Topo";
 
 //const width = Dimensions.get("screen").width;
 
-export default function Cesta({topo, detalhes}) {
+export default function Cesta({topo, detalhes, itens}) {
   return (
-    <>
+    <ScrollView>
       <Topo {...topo} />
       <View style={estilos.cesta}>
         <Detalhes {...detalhes} />
+        <Itens {...itens}/>
       </View>
-    </>
+      </ScrollView>
   );
 }
 
