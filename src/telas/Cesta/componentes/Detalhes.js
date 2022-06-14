49504e50c1fff5ab89bc34cfde6ac009dc34a314
thiagoaderaldo <thiagoaderaldo@gmail.com>
2022-06-14 10:40:14 -0300

    Módulo 4 - Adição do botão comprar

--- src/telas/Cesta/componentes/Detalhes.js
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import { Button, Image, StyleSheet, TouchableOpacity, View } from "react-native";
 import Texto from "../../componentes/Texto";
 
-export default function Detalhes({nome, nomeFazenda, logoFazenda, descricao, preco}) {
+export default function Detalhes({nome, nomeFazenda, logoFazenda, descricao, preco, botao}) {
   return (
     <>
       <Texto style={estilos.nome}>{nome}</Texto>
@@ -13,6 +13,9 @@ export default function Detalhes({nome, nomeFazenda, logoFazenda, descricao, pre
       <Texto style={estilos.descricao}>{descricao}
       </Texto>
       <Texto style={estilos.preco}>{preco}</Texto>
+      <TouchableOpacity style={estilos.botao}>
+        <Texto style={estilos.textoBotao}>{botao}</Texto>
+      </TouchableOpacity>
     </>
   );
 }
@@ -48,5 +51,18 @@ const estilos = StyleSheet.create({
     fontSize: 26,
     lineHeight: 42,
     marginTop: 8,
+  },
+  botao: {
+    marginTop: 16,
+    backgroundColor: "#2A9F85",
+    paddingVertical: 16,
+    borderRadius: 6,
+  },
+  textoBotao:{
+    textAlign: "center",
+    color: "#ffffff",
+    fontSize: 16,
+    lineHeight: 26,
+    fontWeight: "bold",
   },
 });
