2ad01a9b48c93af82b3b8a0925b69ebbcf927f35
thiagoaderaldo <thiagoaderaldo@gmail.com>
2022-06-15 10:07:35 -0300

    Módulo 4 - Implementação de Flatlist

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,27 +1,43 @@
 import React from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
+import Texto from "../componentes/Texto";
 import Detalhes from "./componentes/Detalhes";
-import Itens from "./componentes/Itens";
+import Item from "./componentes/Item";
 
 import Topo from "./componentes/Topo";
 
 //const width = Dimensions.get("screen").width;
 
-export default function Cesta({topo, detalhes, itens}) {
-  return (
-    <ScrollView>
-      <Topo {...topo} />
-      <View style={estilos.cesta}>
-        <Detalhes {...detalhes} />
-        <Itens {...itens}/>
-      </View>
-      </ScrollView>
-  );
+export default function Cesta({ topo, detalhes, itens }) {
+return <>
+    <FlatList
+      data={itens.lista}
+      renderItem={Item}
+      keyExtractor={({ nome }) => nome }
+      ListHeaderComponent={() => {
+        return <>
+          <Topo {...topo} />
+          <View style={estilos.cesta}>
+            <Detalhes {...detalhes} />
+            <Texto style={estilos.titulo}>{ itens.titulo }</Texto>
+          </View>
+        </>
+      }}
+    />
+  </>
 }
 
 const estilos = StyleSheet.create({
   cesta: {
     paddingHorizontal: 16,
     paddingVertical: 8,
+  },
+  titulo: {
+    color: "#464646",
+    fontWeight: "bold",
+    marginTop: 32,
+    marginBottom: 5,
+    fontSize: 20,
+    lineHeight: 32,
   },
 });
